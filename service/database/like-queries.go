a6d32099cb1ed9900580d392f27bca8839090879
rhubi <rhubinger@t-online.de>
2022-12-13 22:10:31 +0100

    IMplemented like queries

--- service/database/like-queries.go
+++ service/database/like-queries.go
@@ -3,29 +3,54 @@ package database
 import "github.com/rhubinger/WASAgram/service/schemes"
 
 func (db *appdbimpl) Like(pid string, uid string) error {
-	return nil
+	_, err := db.c.Exec("INSERT INTO likes VALUES (?, ?);", pid, uid)
+	return err
 }
 
 func (db *appdbimpl) Unlike(pid string, uid string) error {
-	return nil
+	_, err := db.c.Exec("DELETE FROM likes WHERE postId = ? AND userId = ?", pid, uid)
+	return err
 }
 
 func (db *appdbimpl) DeleteLikes(pid string) error {
-	return nil
+	_, err := db.c.Exec("DELETE FROM likes WHERE postId = ?", pid)
+	return err
 }
 
 func (db *appdbimpl) GetLikes(pid string) ([]schemes.User, error) {
-	return []schemes.User{}, nil
+	rows, err := db.c.Query(`SELECT u.userId, u.name, u.posts, u.followers, u.followed 
+							 FROM users u, likes l
+							 WHERE u.userId = l.userId AND l.postId = ?;`, pid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := []schemes.User{}
+	for rows.Next() {
+		u := schemes.User{}
+		err = rows.Scan(&u.UserId, &u.Name, &u.Posts, &u.Followers, &u.Followed)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, err
 }
 
 func (db *appdbimpl) GetLikeCount(pid string) (int, error) {
-	return -1, nil
+	var count int
+	err := db.c.QueryRow("SELECT likes FROM posts WHERE postId = ?", pid).Scan(&count)
+	return count, err
 }
 
 func (db *appdbimpl) IncrementLikeCount(pid string) error {
-	return nil
+	_, err := db.c.Exec("UPDATE posts SET likes = likes + 1 WHERE postId = ?;", pid)
+	return err
 }
 
 func (db *appdbimpl) DecrementLikeCount(pid string) error {
-	return nil
+	_, err := db.c.Exec("UPDATE posts SET likes = likes - 1 WHERE postId = ?;", pid)
+	return err
 }
